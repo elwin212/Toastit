dc2b3f37736acef0a1566c2a6c04ac4f675cfb46
elwin212 <elwin@mmisys.net>
2023-08-03 16:58:05 -0700

    fix auto-signout bug

--- src/components/AutoLogoutProvider.tsx
+++ src/components/AutoLogoutProvider.tsx
@@ -2,60 +2,55 @@
 import React, { useEffect, useState } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 
-
 interface SessionProviderProps {
     children: React.ReactNode;
-
 }
 type WindowActivityEvent = keyof WindowEventMap;  
 
   const LogoutProvider: React.FC<SessionProviderProps> = ({ children }) => {
-    const { data: session, status } = useSession();
-        
+    const { data: session, status } = useSession();    
     const [inactivityTimeout, setInactivityTimeout] = useState<NodeJS.Timeout | null>(null);
     const [userSignedIn, setUserSignedIn] = useState(false);
     
     const resetInactivityTimeout = () => {
         if (inactivityTimeout) {
           clearTimeout(inactivityTimeout);
-        }
-    
+        }        
         // Set a new timeout for auto logout after a period of the of inactivity
-        const newInactivityTimeout = setTimeout(() => {          
+        const newInactivityTimeout = setTimeout(() => {  
+          setUserSignedIn(false);
+          setInactivityTimeout(null);        
           signOut({
-            callbackUrl: `${window.location.origin}/sign-in`,
-          });
-          window.alert('You have been signed out due to inactivity');
-        }, 15 * 60 * 1000); // 15 minutes (in milliseconds)
+            callbackUrl: `${window.location.origin}/`,
+          }).then(() => {
+            window.alert('You have been signed out due to inactivity');
+          });                    
+        }, 30 * 60 * 1000); // 15 minutes (in milliseconds)
     
         setInactivityTimeout(newInactivityTimeout);                
-    };
-       
+    };       
     
       useEffect(() => {
         const handleUserActivity = () => {                  
             resetInactivityTimeout();
-        };
+        };        
         const windowEvents: WindowActivityEvent[] = [
             "keypress",
             "scroll",
             "click"
         ];
-        if(userSignedIn){
-            console.log(userSignedIn,inactivityTimeout);
+        if(userSignedIn){            
             windowEvents.forEach(eventName => {
                 window.addEventListener(eventName, handleUserActivity);
             });
-            
-        
+                    
             return () => {
                 windowEvents.forEach(eventName => {
                     window.removeEventListener(eventName, handleUserActivity);
-                });
-    
+                });    
             };
         }                                        
-      }, [userSignedIn]);
+      }, [userSignedIn,inactivityTimeout]);
   
     // Check if the user is logged in on mount
     
