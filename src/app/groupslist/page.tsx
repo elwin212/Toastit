89e34847885c9c4499b76010ffeb717dec62ca3e
elwin212 <elwin@mmisys.net>
2023-08-03 16:33:25 -0700

    add back button

--- src/app/groupslist/page.tsx
+++ src/app/groupslist/page.tsx
@@ -1,3 +1,4 @@
+import ToFeedButton from "@/components/ToFeedButtom";
 import { db } from "@/lib/db";
 import axios from "axios";
 import Image from "next/image";
@@ -11,16 +12,20 @@ const Page = async () => {
         },
         include: {
             subscribers: true,
+            posts: true,
         },
         take: 50,
     });
     
-    return (
-        <div className="container flex items-center h-full max-w-2xl mx-auto">
-                <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">
+    return (       
+        <>
+        <div><ToFeedButton /></div>
+        <div className="container flex items-center h-full max-w-2xl mx-auto">              
+                <div className="relative bg-white w-full h-fit p-4 rounded-lg space-y-6">                                    
                     <div className="flex justify-between items-center">
-                        <h1 className="text-xl font-semibold">Groups</h1>
-                    </div>
+                        <h1 className="text-xl font-semibold mb-1">Groups</h1> 
+                        <p className="text-sm text-zinc-400">Choose a Group to subscribe!</p>                       
+                    </div>                    
                     <hr className='bg-red-500 h-px' />                    
                     {groups.map((group) => (
                         <div key={group.id} className="flex items-center justify-between">
@@ -33,9 +38,10 @@ const Page = async () => {
                                 height={30}
                                 className="h-8 w-8 sm:h-6 sm:w-6"
                             />                                                            
-                            <Link href={`/r/${group.name}`} className="ml-2 text-blue-500 hover:underline">
+                            <Link href={`/r/${group.name}`} className="text-lg ml-2 text-black hover:underline">
                                 {group.name}
-                            </Link>           
+                            </Link>
+                            <p className="text-sm text-zinc-500">&nbsp;&nbsp;-&nbsp;&nbsp;{group.posts.length} posts</p>           
                             </div>
                             <div className="flex items-center">                 
                             <span >Members: {group.subscribers.length}</span>
@@ -44,6 +50,7 @@ const Page = async () => {
                     ))}
                 </div>
         </div>
+        </>
     );
 }
 
