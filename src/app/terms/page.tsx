346cc8d2cbf04636b49a11ca8c645a15f3b97b93
elwin212 <elwin@mmisys.net>
2023-08-02 20:56:32 -0700

    add temp page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from 'next/navigation'
+import { authOptions, getAuthSession } from '@/lib/auth'
+
+export const metadata = {
+  title: 'Profile',
+  description: 'User profile page',
+}
+
+export default async function SettingsPage() {
+
+  return (
+    <div className='max-w-4xl mx-auto py-12'>
+      <div className='grid items-start gap-8'>
+        <h1 className='font-bold text-3xl md:text-4xl'>Terms</h1>
+
+        <div className='grid gap-10'>
+            <h2>This page is still in progress...</h2>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
