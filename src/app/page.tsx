d9ddda0ea52dce9f69133b01664864766b06f44e
elwin212 <elwin@mmisys.net>
2023-08-03 18:14:13 -0700

    update prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
             <HomeIcon className="w-4 h-4"/>
             Home
           </p>
-          <p className="font-semibold text-sm text-zinc-500">This website is still in progress, stay tuned!</p>
+          <p className="font-semibold text-sm text-zinc-500">Wait for more features, stay tuned!</p>
         </div>
 
         <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
